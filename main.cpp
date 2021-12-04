7f7ab68df63c307ab7dd79b99b6271a1edf945e6
Augusto Pieper <augustoopieper@gmail.com>
2021-12-03 21:27:47 -0300

    gameplay improvement

--- main.cpp
+++ main.cpp
@@ -3,113 +3,130 @@
 
 class Player {
 public:
-	Vector2 playerPosition;
-	int score;
-	void draw(Vector2 midScreenVec) {
-		DrawRectangleV(midScreenVec, {10, 60}, WHITE);
-	}
+  Vector2 playerPosition;
+  int score;
+  void draw(Vector2 midScreenVec) {
+    DrawRectangleV(midScreenVec, {10, 60}, WHITE);
+  }
 };
 
 Vector2 generateBallSpeed() {
-	Vector2 ballSpeed;
-	if ((rand() % 2) == 0) {
-		ballSpeed.x = 6;
-	} else {
-		ballSpeed.x = -6;
-	}
-	if ((rand() % 2) == 0) {
-		ballSpeed.y = rand() % -4 + 4;
-	} else {
-		ballSpeed.y = rand() % 4 - 4;
-	}
-	return ballSpeed;
+  Vector2 ballSpeed;
+  if ((rand() % 2) == 0) {
+    ballSpeed.x = 6;
+  } else {
+    ballSpeed.x = -6;
+  }
+  ballSpeed.y = rand() % -2 + 2;
+  // if ((rand() % 2) == 0) {
+  //   ballSpeed.y = rand() % -2 + 2;
+  // } else {
+  //   ballSpeed.y = rand() % 2 - 2;
+  // }
+  return ballSpeed;
 }
 
 int main(void) {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+  const int screenWidth = 800;
+  const int screenHeight = 450;
 
-	InitWindow(screenWidth, screenHeight, "pong");
+  InitWindow(screenWidth, screenHeight, "pong");
 
-	SetTargetFPS(60);
+  SetTargetFPS(60);
 
-	Vector2 ballPosition = {(float)screenWidth / 2 - 5, (float)screenHeight / 2 - 5};
-	Vector2 ballSpeed = generateBallSpeed();
-	
-	int ballRadius = 10;
+  Vector2 ballPosition = {(float)screenWidth / 2 - 5,
+                          (float)screenHeight / 2 - 5};
+  Vector2 ballSpeed = generateBallSpeed();
 
-	Player player1;
-	player1.score = 0;
-	player1.playerPosition = {60, (float)screenHeight / 2 - 30};
-	Player player2;
-	player2.score = 0;
-	player2.playerPosition = {screenWidth - 60, (float)screenHeight / 2 - 30};
+  int ballRadius = 10;
 
-	bool pause = 0;
+  Player player1;
+  player1.score = 0;
+  player1.playerPosition = {60, (float)screenHeight / 2 - 30};
+  Player player2;
+  player2.score = 0;
+  player2.playerPosition = {screenWidth - 60, (float)screenHeight / 2 - 30};
 
-	while (!WindowShouldClose()) {
+  bool pause = 0;
 
-		if (IsKeyDown(KEY_SPACE))
-			pause = 0;
+  while (!WindowShouldClose()) {
 
-		if (!pause) {
-			if (IsKeyDown(KEY_UP))
-				player2.playerPosition.y -= 6.0f;
-			if (IsKeyDown(KEY_DOWN))
-				player2.playerPosition.y += 6.0f;
-			if (IsKeyDown(KEY_W))
-				player1.playerPosition.y -= 6.0f;
-			if (IsKeyDown(KEY_S))
-				player1.playerPosition.y += 6.0f;
+    if (IsKeyDown(KEY_SPACE))
+      pause = 0;
 
-			ballPosition.x += ballSpeed.x;
-			ballPosition.y += ballSpeed.y;
-			if ((ballPosition.y >= (screenHeight - ballRadius)) ||
-					(ballPosition.y <= ballRadius))
-				ballSpeed.y *= -1.0f;
-			if (ballPosition.x >= (screenWidth - ballRadius)) {
-				player1.score += 1;
-				ballPosition = {(float)screenWidth / 2 - 5, (float)screenHeight / 2 - 5};
-				ballSpeed = generateBallSpeed();
-				pause = 1;
-			}
-			else if (ballPosition.x <= -ballRadius) {
-				player2.score += 1;
-				ballPosition = {(float)screenWidth / 2 - 5, (float)screenHeight / 2 - 5};
-				ballSpeed = generateBallSpeed();
-				pause = 1;
-			}
+    if (!pause) {
+      if (IsKeyDown(KEY_UP))
+        player2.playerPosition.y -= 10.0f;
+      if (IsKeyDown(KEY_DOWN))
+        player2.playerPosition.y += 10.0f;
+      if (IsKeyDown(KEY_W))
+        player1.playerPosition.y -= 10.0f;
+      if (IsKeyDown(KEY_S))
+        player1.playerPosition.y += 10.0f;
 
-			if (ballPosition.x >= (player1.playerPosition.x - ballRadius) &&
-					ballPosition.x <= (player1.playerPosition.x + 10) &&
-					ballPosition.y >= (player1.playerPosition.y - ballRadius) &&
-					ballPosition.y <= (player1.playerPosition.y + 60)) {
-				ballSpeed.x *= -1.0f;
-			}
-			if (ballPosition.x >= (player2.playerPosition.x - 10) &&
-					ballPosition.x <= (player2.playerPosition.x + ballRadius) &&
-					ballPosition.y >= (player2.playerPosition.y - ballRadius) &&
-					ballPosition.y <= (player2.playerPosition.y + 60)) {
-				ballSpeed.x *= -1.0f;
-			}
-		}
+      if (player1.playerPosition.y < 0) {
+        player1.playerPosition.y = 0;
+      }
+      if (player1.playerPosition.y > screenHeight - 60) {
+        player1.playerPosition.y = screenHeight - 60;
+      }
+      if (player2.playerPosition.y < 0) {
+        player2.playerPosition.y = 0;
+      }
+      if (player2.playerPosition.y > screenHeight - 60) {
+        player2.playerPosition.y = screenHeight - 60;
+      }
 
-		BeginDrawing();
+      ballPosition.x += ballSpeed.x;
+      ballPosition.y += ballSpeed.y;
+      if ((ballPosition.y >= (screenHeight - ballRadius)) ||
+          (ballPosition.y <= ballRadius))
+        ballSpeed.y *= -1.0f;
+      if (ballPosition.x >= (screenWidth - ballRadius)) {
+        player1.score += 1;
+        ballPosition = {(float)screenWidth / 2 - 5,
+                        (float)screenHeight / 2 - 5};
+        ballSpeed = generateBallSpeed();
+        pause = 1;
+      } else if (ballPosition.x <= -ballRadius) {
+        player2.score += 1;
+        ballPosition = {(float)screenWidth / 2 - 5,
+                        (float)screenHeight / 2 - 5};
+        ballSpeed = generateBallSpeed();
+        pause = 1;
+      }
 
-		ClearBackground(BLACK);
+      if (ballPosition.x >= (player1.playerPosition.x - ballRadius) &&
+          ballPosition.x <= (player1.playerPosition.x + 10) &&
+          ballPosition.y >= (player1.playerPosition.y - ballRadius) &&
+          ballPosition.y <= (player1.playerPosition.y + 60)) {
+        ballSpeed.x *= -1.1f;
+      }
 
-		DrawText(TextFormat("%d || %d", player1.score, player2.score), 20, 20, 28,
-						 WHITE);
+      if (ballPosition.x >= (player2.playerPosition.x - 10) &&
+          ballPosition.x <= (player2.playerPosition.x + ballRadius) &&
+          ballPosition.y >= (player2.playerPosition.y - ballRadius) &&
+          ballPosition.y <= (player2.playerPosition.y + 60)) {
+        ballSpeed.x *= -1.1f;
+      }
+    }
 
-		player1.draw(player1.playerPosition);
-		player2.draw(player2.playerPosition);
+    BeginDrawing();
 
-		DrawCircleV(ballPosition, ballRadius, WHITE);
+    ClearBackground(BLACK);
 
-		EndDrawing();
-	}
+    DrawText(TextFormat("%d | %d", player1.score, player2.score), 20, 20, 28,
+             WHITE);
 
-	CloseWindow();
+    player1.draw(player1.playerPosition);
+    player2.draw(player2.playerPosition);
 
-	return 0;
+    DrawCircleV(ballPosition, ballRadius, WHITE);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
 }
